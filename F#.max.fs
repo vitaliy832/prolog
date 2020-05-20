0cc9c253dda06dc5826ed910ef5e9432e92f1478
vitaliy832 <62376637+vitaliy832@users.noreply.github.com>
2020-05-20 13:16:29 +0300

    Add files via upload

---
+++ F#.max.fs
@@ -0,0 +1,15 @@
+﻿let max2 x y =
+   if x>y then x
+   else y
+
+let p1_1 = 
+  let b = System.Convert.ToInt32(System.Console.ReadLine())
+  let c = System.Convert.ToInt32(System.Console.ReadLine())
+  max2 b c
+
+[<EntryPoint>]
+let main argv =
+ let Ans=p1_1
+ System.Console.Write(Ans)
+ let z = System.Console.ReadKey()
+ 0
