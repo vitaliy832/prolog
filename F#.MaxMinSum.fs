aad75d0bb8be8192f03e1bced316da77bbafaa2f
vitaliy832 <62376637+vitaliy832@users.noreply.github.com>
2020-05-20 14:55:39 +0300

    Add files via upload

---
+++ F#.MaxMinSum.fs
@@ -0,0 +1,25 @@
+﻿let max2 x y =
+    if x>y then x
+    else y
+let min2 x y =
+    if x<y then x
+    else y
+let sum2 x y = x+y
+let p3 f x y z = 
+  let f2 = f x y
+  f f2 z
+
+
+
+let p1_2 = 
+    let a= System.Convert.ToInt32(System.Console.ReadLine())
+    let b= System.Convert.ToInt32(System.Console.ReadLine())
+    let c= System.Convert.ToInt32(System.Console.ReadLine())
+    (p3 max2 a b c, p3 min2 a b c, p3 sum2 a b c)
+
+[<EntryPoint>]
+let main argv = 
+    let Ans = p1_2
+    System.Console.Write(Ans)
+    let z = System.Console.ReadKey() 
+    0
