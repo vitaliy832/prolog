d984482578398d4c354ee5c4e5ee6f26870c5bdb
vitaliy832 <62376637+vitaliy832@users.noreply.github.com>
2020-05-20 13:49:40 +0300

    Add files via upload

---
+++ F#.sq.fs
@@ -0,0 +1,19 @@
+﻿let y b =
+ b*b
+
+[<EntryPoint>]
+ let main argv =
+
+
+
+  let solve a b c =
+    let D = b*b - 4.0*a*c
+    ((-b-sqrt(D))/(2.0*a),(-b-sqrt(D))/(2.0*a))
+  printfn "%A%A" solve (1.0, 2.0, -3.0)
+
+  let x = y 10 in 
+     printfn "%i" x
+  let z = System.Console.ReadKey()
+
+  printfn "%A" argv
+  0
