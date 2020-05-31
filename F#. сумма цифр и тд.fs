7e750e8705af76fb015e4aa6d8f7023b4a39c66e
vitaliy832 <62376637+vitaliy832@users.noreply.github.com>
2020-05-31 15:08:54 +0300

    Add files via upload

---
+++ F#. сумма цифр и тд.fs
@@ -0,0 +1,31 @@
+﻿let max2 x y =
+    if x>y then x
+    else y
+let min2 x y =
+    if x<y then x
+    else y
+let sum2 x y = x+y
+let pr x y = x*y
+
+
+let rec cifri f n init=
+    if n=0 then init
+    else
+        let ost = n%10
+        f ost (cifri f (n/10)init)
+
+
+
+let p9_7 = 
+    let x= System.Convert.ToInt32(System.Console.ReadLine())
+    (cifri sum2 x 0, cifri pr x 1, cifri max2 x 0, cifri min2 x 10)
+
+[<EntryPoint>]
+let main argv = 
+    let Ans = p9_7
+    System.Console.Write(Ans)
+    let z = System.Console.ReadKey() 
+
+    printfn "%A" argv
+    0
+
