b4f80a09619071e51de61a997e395904a1827b25
vitaliy832 <62376637+vitaliy832@users.noreply.github.com>
2020-05-31 12:40:55 +0300

    Add files via upload

---
+++ простые числа.fs
@@ -0,0 +1,23 @@
+﻿let non_del X Y =
+    if X % Y =0 then false
+                else true
+
+let rec pros X Y =
+    if X=Y then true
+    else (non_del X Y) && (pros X (Y+1))
+
+let pr n =
+    match n with
+    |2->true
+    |_->pros n 2
+
+let p9_5 =
+    let a = System.Convert.ToInt32(System.Console.ReadLine())
+    pr a
+
+[<EntryPoint>]
+let main argv =
+    let Ans = p9_5
+    System.Console.Write(Ans)
+    let z = System.Console.ReadKey()
+    0
