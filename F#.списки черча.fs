4689265a0691d6fd86ad6180e52a66f5fb4aca33
vitaliy832 <62376637+vitaliy832@users.noreply.github.com>
2020-06-01 20:29:12 +0300

    Add files via upload

---
+++ F#.списки черча.fs
@@ -0,0 +1,38 @@
+﻿let rec read_list n =
+    if n=0 then []
+    else
+    let Head = System.Convert.ToInt32(System.Console.ReadLine())
+    let Tail = read_list (n-1)
+    Head::Tail
+
+
+
+let read_data = 
+    let n=System.Convert.ToInt32(System.Console.ReadLine())
+    read_list n
+
+
+
+let prov H = H%2=0
+
+
+let rec prog2 prov h t =
+    match t with
+    |[] -> h
+    | head::tail -> if ((prov h)=(prov head))&&(head>h) then prog2 prov head tail
+                                                        else prog2 prov h tail
+let prog list = 
+    match list with
+    |[]->0
+    |head::tail -> prog2 prov head tail
+
+
+
+[<EntryPoint>]
+let main argv =
+    let l = read_data
+    let Ans=prog l
+    System.Console.WriteLine(Ans)
+    let z=System.Console.ReadKey()
+    printfn "%A" argv
+    0
